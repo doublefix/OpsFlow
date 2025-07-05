3a08ae6af016f77af065faa2517a3ef6da9addc2
mahongqin <codepapercut@gmail.com>
2025-07-05 22:17:21 +0800

    Fix s3

--- cmd/s3/main.go
+++ cmd/s3/main.go
@@ -51,10 +51,4 @@ func main() {
 	}
 }
 
-// mc alias set myminio https://s3.openpaper.co:9000 ccc ccc
-
 // mc ls s3/ai-models/huggingface/google-t5/t5-base/161aed0c/
-// mc mirror s3/ai-models/huggingface/google-t5/t5-base/161aed0c/ ./t5-base/
-
-// mc ls s3/ai-models/huggingface/prajjwal1/bert-tiny/5fc19a73/
-// mc mirror s3/ai-models/huggingface/prajjwal1/bert-tiny/5fc19a73/ ./bert-tiny/
